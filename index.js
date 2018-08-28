1c3252305ba77369af775f645470ca274e632129
na59ri <32233567+na59ri@users.noreply.github.com>
2018-08-28 23:50:46 +0900

    id

--- index.js
+++ index.js
@@ -95,8 +95,8 @@ function sendStamp(userId) {
     console.log("sendStamp:" + userId);
     bot.pushMessage(userId, {
         type: "sticker",
-        packageId: 1,
-        stickerId: 6
+        packageId: "1",
+        stickerId: "6"
     });
 }
 
