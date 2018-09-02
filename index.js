5b22e9a544cc77bfab030a3a5902f2f02ab51feb
na59ri <32233567+na59ri@users.noreply.github.com>
2018-09-02 13:35:54 +0900

    modify syntax error

--- index.js
+++ index.js
@@ -138,7 +138,8 @@ server.post('/bot/webhook', line.middleware(line_config), (req, res, next) => {
                 let result = event.message.text.match(/@(.+)[ ]*[\n|\r]+(.+)/);
                 if (result && 0 < result.length) {
                     console.log(result);
-                    console.log(await translator.translator(result[2]));
+                    let trans = await translator.translator(result[2]);
+                    console.log(trans);
 
                     // 返信がない場合に向けに、タイマーを設定
                     let userId = checkUserName(groupId, result[1]);
