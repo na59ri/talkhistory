c37feabadd13a05d0264875084a913d48830487a
na59ri <32233567+na59ri@users.noreply.github.com>
2018-09-11 23:17:16 +0900

    log all comment

--- index.js
+++ index.js
@@ -206,7 +206,7 @@ server.post('/bot/webhook', line.middleware(line_config), (req, res, next) => {
     req.body.events.forEach((event) => {
 
 
-        console.log("[beacon]" + JSON.stringify(event));
+        console.log("[all]]" + JSON.stringify(event));
         // この処理の対象をイベントタイプがメッセージで、かつ、テキストタイプだった場合に限定。
         if (event.type == "message" && event.message.type == "text") {
 
