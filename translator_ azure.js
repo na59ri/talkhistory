3a801f866d8de48211ce40bb6944c12381244ff1
na59ri <32233567+na59ri@users.noreply.github.com>
2018-10-06 23:34:31 +0900

    add azure

---
+++ translator_ azure.js
@@ -0,0 +1,36 @@
+const https = require('https');
+
+const options = {
+    protocol: 'https:',
+    host: 'api.cognitive.microsoft.com',
+    path: 'sts/v1.0',
+    method: 'POST',
+    headers: {
+        'Content-Type': 'application/json',
+        'Accept': 'application/jwt',
+        'Ocp-Apim-Subscription-Key': process.env.AZURE_TRANLATOR_KEY
+    }
+};
+
+
+module.exports.translator = translator;
+
+
+async function translator(groupId, name, text, analyzer) {
+
+    const req = https.request(options, (res) => {
+        res.on('data', (chunk) => {
+            console.log(`BODY: ${chunk}`);
+        });
+        res.on('end', () => {
+            console.log('No more data in response.');
+        });
+    })
+
+    req.on('error', (e) => {
+        console.error(`problem with request: ${e.message}`);
+    });
+
+    req.end();
+
+}
